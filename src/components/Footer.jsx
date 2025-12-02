e61d38c0d24f53bbd2e2e2338e73921ad626e025
Benjamin Simon <benjamin51.simon@gmail.com>
2025-12-02 19:48:09 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,12 +10,12 @@ const socialLinks = [
 const Footer = () => {
   return (
     <footer className="w-screen bg-[#5542ff] py-4 text-black">
-      <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
-        <p className="text-center text-sm font-light md:text-left">
-          @BNJ 2024. All rights reserved
+      <div className="container flex flex-col items-center justify-between gap-4 px-4 mx-auto md:flex-row">
+        <p className="text-sm font-light text-center md:text-left">
+          @BNJ 2025. All rights reserved
         </p>
 
-        <div className="flex justify-center gap-4  md:justify-start">
+        <div className="flex justify-center gap-4 md:justify-start">
           {socialLinks.map((link, index) => (
             <a
               key={index}
@@ -31,7 +31,7 @@ const Footer = () => {
 
         <a
           href="#privacy-policy"
-          className="text-center text-sm font-light hover:underline md:text-right"
+          className="text-sm font-light text-center hover:underline md:text-right"
         >
           Privacy Policy
         </a>
